3ad36b5b421a88b7e72b191a9148f8d93f1d934c
Anton Karpov <antonkarpov91@yandex.ru>
2026-01-16 22:55:27 +0300

    ver1

---
+++ consumer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.31.125'))
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+
+channel.basic_consume(callback, queue='hello', no_ack=True)
+channel.start_consuming()
